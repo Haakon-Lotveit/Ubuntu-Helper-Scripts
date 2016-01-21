be426ba4a4b99de7ede0490b17601e8296ce06a1
Haakon Løtveit <haakon_lotveit@live.no>
2016-01-21 23:58:50 +0100

    cleanup of emacs-setup file

--- setup-all.sh
+++ setup-all.sh
@@ -18,16 +18,6 @@
 # Step 3: Setup emacs #
 #######################
 
-pushd .. > /dev/null
-git clone https://github.com/Haakon-Lotveit/emacs-lisp-odds-and-sods
-
-pushd emacs-lisp-odds-and-sods > /dev/null
-chmod +x ./setup.sh
-. setup.sh
-
-popd > /dev/null
-popd > /dev/null
-
 . setup-emacs.sh
 
 echo "Eclipse and Lisp needs to be set up manually"
--- setup-emacs.sh
+++ setup-emacs.sh
@@ -4,11 +4,22 @@
 # Downloads Emacs customizations from my github repo and links it properly.
 #
 
+
+pushd .. > /dev/null
+git clone https://github.com/Haakon-Lotveit/emacs-lisp-odds-and-sods
+
+pushd emacs-lisp-odds-and-sods > /dev/null
+chmod +x ./setup.sh
+. setup.sh
+
+popd > /dev/null
+popd > /dev/null
+
 pushd ~
 
 rm -rf .emacs.d
 
-#link
+# create a symbolic link
 ln ~/git/emacs-lisp-odds-and-sods/dotemacsdotd ~/.emacs.d --symbolic
 
 popd
